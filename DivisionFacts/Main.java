10abb7b70fac06f44ba863bf6fb1c86176a62840
Aaron-coderson <51865907+Aaron-coderson@users.noreply.github.com>
2020-06-15 17:39:18 -0700

    tt

---
+++ DivisionFacts/Main.java
@@ -0,0 +1,98 @@
+
+/**
+ * Write a description of class Main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+
+    // Summer Trip
+
+    Scanner scan = new Scanner(System.in);
+
+  System.out.println("Today we will be doing a summer trip Mad Lib");
+
+  System.out.print("Name a person ");
+
+  String scan1 = scan.nextLine();
+
+  System.out.print("Name a place(it can be anywhere. even the closet) ");
+
+  String scan2 = scan.nextLine();
+
+  System.out.print("Name a adjective ");
+
+  String scan3 = scan.nextLine();
+
+  String scan4 = scan2;
+
+  System.out.print("Name a plural noun ");
+
+  String scan5 = scan.nextLine();
+
+  System.out.print("Name an adjective ");
+
+  String scan6 = scan.nextLine();
+
+  System.out.print("Name a plural noun ");
+
+  String scan7 = scan.nextLine();
+
+  System.out.print("Name a place(it can be anywhere. even the underneath the couch) ");
+
+  String scan8 = scan.nextLine();
+  
+  System.out.print("Name an action verb ");
+
+  String scan9 = scan.nextLine();
+
+  System.out.print("Name a plural noun ");
+
+  String scan10 = scan.nextLine();
+
+  System.out.print("Name a plural noun ");
+
+  String scan11 = scan.nextLine();
+
+  System.out.print("Name a noun ");
+
+  String scan12 = scan.nextLine();
+
+  System.out.print("Name an action verb ");
+
+  String scan13 = scan.nextLine();
+  
+  System.out.print("Name a noun ");
+
+  String scan14 = scan.nextLine();
+
+  System.out.print("Name an adjective ");
+
+  String scan15 = scan.nextLine();
+
+  System.out.println("Last summer, my mom and dad took me and " + scan1 + " on a trip to " + scan2 + ". The weather there was is very " + scan3 + "! Northern " + scan4 + " has many " + scan5 + ", and they make " + scan6 + " " + scan7 + " there. Many people also go to " + scan8 + " to " + scan9 + " or see the " + scan10 + ". The people that live there love to eat the " + scan11 + " and are very proud of their big " + scan12 + ". They also like to " + scan13 +" in the sun and swim in the " + scan14 +"! It was a realy " + scan15 + " trip!");
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
