6f9dd22e17a6f9e5aa655ce0b3be16f74dfc599a
Aaron-coderson <51865907+Aaron-coderson@users.noreply.github.com>
2020-07-13 17:23:40 -0700

    as per class 7/13/2020

--- Loops(JS4)/paintball.java
+++ Loops(JS4)/paintball.java
@@ -13,39 +13,39 @@ public class paintball extends JApplet
     
     
  public void paint(Graphics j) {
-//    //for loop
-//    //left -> right
-//      for(int z = 0; z<=700; z = z+100){
-//          j.setColor(Color.red);
-//          j.fillOval(z,z,100,100);
-//         
-//     }
-//     //right->left
-//     int g = 0;
-//     for(int k = 700; k>=0; k = k-100){
-//          j.setColor(Color.blue);
-//          j.fillOval(k,g,100,100);
-//          g = g +100;
-//         
-//     }
-//     
-    //while loop
-    //left -> roght
-    int h = 0;
-    while(h<=700){
-        j.setColor(Color.red);
-        j.fillOval(h,h,100,100);
-        h = h + 100;
+   //for loop
+   //left -> right
+     for(int z = 0; z<=700; z = z+100){
+         j.setColor(Color.red);
+         j.fillOval(z,z,100,100);
+        
+    }
+    //right->left
+    int g = 0;
+    for(int k = 700; k>=0; k = k-100){
+         j.setColor(Color.blue);
+         j.fillOval(k,g,100,100);
+         g = g +100;
+        
     }
     
-    int f = 700;
-    int u = 0;
-    while(f>=0){
-        j.setColor(Color.blue);
-        j.fillOval(f,u,100,100);
-        f = f -100;
-        u = u +100;
-    }
+//     //while loop
+//     //left -> roght
+//     int h = 0;
+//     while(h<=700){
+//         j.setColor(Color.red);
+//         j.fillOval(h,h,100,100);
+//         h = h + 100;
+//     }
+//     
+//     int f = 700;
+//     int u = 0;
+//     while(f>=0){
+//         j.setColor(Color.blue);
+//         j.fillOval(f,u,100,100);
+//         f = f -100;
+//         u = u +100;
+//     }
     
     
     
